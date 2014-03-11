8e0e0b4a868455a9bc5a8c7d8f2d21ed620e4ce0
VincentBorchardt <vborchardt123@gmail.com>
2014-03-10 22:15:52 -0500

    added basic functions, created gui namespace

--- mtg-reporter-replacement/bin/mtg_reporter_replacement/core_test.clj
+++ mtg-reporter-replacement/bin/mtg_reporter_replacement/core_test.clj
@@ -2,6 +2,29 @@
   (:use clojure.test
         mtg-reporter-replacement.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
+(def sample-player {:first "Sample", :last "Player", :number 123})
+(def second-player {:first "Second", :last "Person", :number 456})
+(def match-result-one {:round 1, :playerA 123, :playerB 456, :a-wins 2, :b-wins 1, :draws 0})
+(def match-result-two {:round 1, :playerA 123, :playerB 456, :a-wins 1, :b-wins 2, :draws 0})
+(def match-result-three {:round 1, :playerA 123, :playerB 456, :a-wins 1, :b-wins 1, :draws 1})
+
+(deftest create-player-test
+  (testing "creating players works"
+           (is (= (create-player "Sample" "Player" 123) sample-player))))
+
+(deftest create-match-result-test
+  (testing "creating match results works"
+           (is (= (create-match-result 1 sample-player second-player 2 1 0) match-result-one))))
+
+(deftest calculate-match-points-test
+  (testing "creating match points works"
+           (is (= (calculate-match-points sample-player match-result-one) 3))
+           (is (= (calculate-match-points sample-player match-result-two) 0))
+           (is (= (calculate-match-points sample-player match-result-three) 1))
+           (is (= (calculate-match-points second-player match-result-one) 0))
+           (is (= (calculate-match-points second-player match-result-two) 3))
+           (is (= (calculate-match-points second-player match-result-three) 1))))
+
--- mtg-reporter-replacement/src/mtg_reporter_replacement/core.clj
+++ mtg-reporter-replacement/src/mtg_reporter_replacement/core.clj
@@ -5,4 +5,33 @@
   [x]
   (println x "Hello, World!"))
 
-(def sample-player {:first "Sample", :last "Player", :number 123})
+;Players need first and last names, along with DCI number
+;I think they want country eventually, but that can come later
+(defn create-player [first last num]
+  {:first first, :last last, :number num})
+
+;Match results have a round number, the two players involved
+;(identified by number since each player's round results are different under Clojure),
+;each players wins and draws--the points calculation can be done outside match results,
+;which extends to 3/5 and similiar stuff
+(defn create-match-result [round-num playerA playerB first-wins second-wins draws]
+  {:round round-num, :playerA (playerA :number), :playerB (playerB :number),
+   :a-wins first-wins, :b-wins second-wins, :draws draws})
+
+;This gives the match points for any specific result:
+;3 points for a win (I have more wins than you)
+;1 point for a draw (same amount of wins as you)
+;0 points for a loss (fewer wins than you)
+;I need to customize the default results (that's used in both result lists) to do so easily
+(defn personal-match-result [player match-result]
+  (if (= (player :number) (match-result :playerA))
+    {:wins (match-result :a-wins) :losses (match-result :b-wins) :draws (match-result :draws)}
+    {:wins (match-result :b-wins) :losses (match-result :a-wins) :draws (match-result :draws)}))
+
+(defn calculate-match-points [player match-result]
+  (let [res (personal-match-result player match-result)]
+    (cond
+      (> (res :wins) (res :losses)) 3
+      (< (res :wins) (res :losses)) 0
+      :else 1)))
+  
