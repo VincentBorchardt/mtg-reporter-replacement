d6cd852d9d342226a67f201395eedd75c6056e64
VincentBorchardt <vborchardt123@gmail.com>
2014-03-14 20:13:10 -0500

    lots of new gui stuff, still in preliminary stage

--- mtg-reporter-replacement/src/mtg_reporter_replacement/gui.clj
+++ mtg-reporter-replacement/src/mtg_reporter_replacement/gui.clj
@@ -1,2 +1,20 @@
-(ns mtg-reporter-replacement.gui)
+(ns mtg-reporter-replacement.gui
+  (:use seesaw.core
+        mtg-reporter-replacement.core
+        mtg-reporter-replacement.results-gui))
+
+(defn -main [& args]
+  (invoke-later
+    (-> results-frame
+     pack!
+     show!)))
+
+(defn basic [& args]
+  (invoke-later
+    (-> (frame :title "Hello",
+           :content "Hello, Seesaw",
+           :on-close :exit)
+     pack!
+     show!)))
+
 
---
+++ mtg-reporter-replacement/src/mtg_reporter_replacement/results_gui.clj
@@ -0,0 +1,20 @@
+(ns mtg-reporter-replacement.results-gui)
+
+(def b (button :text "Some Text"))
+(def c (button :text "More Text"))
+
+(listen b :action #(alert % "response"))
+
+(def buttons-panel (flow-panel
+    :align :left
+    :hgap 20
+    :items [b c]))
+
+(def results-content
+  (border-panel :hgap 10 :vgap 10
+                :south buttons-panel
+                :center "Center!"))
+
+(def results-frame 
+  (frame :title "Results Frame"
+         :content results-content))
