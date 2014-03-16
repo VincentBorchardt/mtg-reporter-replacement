c923627e088d723a315c9f80af377c72a0ee2969
VincentBorchardt <vborchardt123@gmail.com>
2014-03-16 16:53:22 -0500

    More experimental GUI stuff, created a controller for results

--- mtg-reporter-replacement/src/mtg_reporter_replacement/results_gui.clj
+++ mtg-reporter-replacement/src/mtg_reporter_replacement/results_gui.clj
@@ -1,20 +1,33 @@
 (ns mtg-reporter-replacement.results-gui
-  (:use seesaw.core))
+  (:use seesaw.core
+        seesaw.table))
 
-(def b (button :text "Some Text"))
-(def c (button :text "More Text"))
+(def two-zero-button (button :text "2-0"))
+(def two-one-button (button :text "2-1"))
+(def one-two-button (button :text "1-2"))
+(def zero-two-button (button :text "0-2"))
 
-(listen b :action #(alert % "response"))
+;(listen b :action #(alert % "response"))
+
+(def pairings-listbox (listbox :model ["item 1" "item 2" "item 3"]))
+
+;this doesn't show headings even though I think it should?
+(def pairings-table 
+  (table :model
+         [:columns [{:key :player1, :name "Player 1"} :player2]
+          :rows '[["test 1" "test 2"]
+                  ["test 3" "test 4"]
+                  {:player1 "test 5" :player2 "test 6"}]]))
 
 (def buttons-panel (flow-panel
     :align :left
     :hgap 20
-    :items [b c]))
+    :items [two-zero-button two-one-button one-two-button zero-two-button]))
 
 (def results-content
   (border-panel :hgap 10 :vgap 10
                 :south buttons-panel
-                :center "Center!"))
+                :center pairings-table))
 
 (def results-frame 
   (frame :title "Results Frame"
